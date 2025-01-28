c2be9df3741456fef0afc08e1b08c88ee2fc8576
unknown <smourya7425@gmail.com>
2025-01-28 14:21:46 +0530

    wip

---
+++ src/components/Button/buttion.test.tsx
@@ -0,0 +1,19 @@
+import Button from './index';
+
+describe(('Button unit test') => {
+  it('should exist in the DOM', () => {
+    render(<Button />)
+  })
+  it('should have onClick handler', () => {
+    render(<Button />)
+  })
+  it('should have label', () => {
+    render(<Button />)
+  })
+  it('should have disabled prop', () => {
+    render(<Button />)
+  })
+  it('should have varient prop', () => {
+    render(<Button />)
+  })
+})
